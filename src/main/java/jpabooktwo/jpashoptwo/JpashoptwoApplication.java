ee94d9502632176798fed3ebbadab9587bd05416
ksungsu <zxzxzlxlzlxl@naver.com>
2022-01-03 12:02:28 +0900

    70th TIL jpashop 복습

---
+++ src/main/java/jpabooktwo/jpashoptwo/JpashoptwoApplication.java
@@ -0,0 +1,14 @@
+package jpabooktwo.jpashoptwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpashoptwoApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(JpashoptwoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jpabooktwo/jpashoptwo/JpashoptwoApplicationTests.java
@@ -0,0 +1,13 @@
+package jpabooktwo.jpashoptwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpashoptwoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
